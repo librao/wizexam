285361799de86ab459b9a1023b437305c8204f38
Lucky <libtowork@gmail.com>
2013-03-18 00:09:40 +0800

    digg fun finish

--- Lib/Model/NewsModel.class.php
+++ Lib/Model/NewsModel.class.php
@@ -4,22 +4,22 @@ class NewsModel extends Model {
 
     const STATUS_ACTIVE = 'ACTIVE';
 
-protected $_auto = array(
+    protected $_auto = array(
         array('status', self::STATUS_ACTIVE),
         array('created_at', 'getCurrentDateTime', Model::MODEL_INSERT, 'function'),
         array('updated_at', 'getCurrentDateTime', Model::MODEL_BOTH, 'function')
     );
 
-	/**
-	 * 获得最新新闻
-	 * 
-	 * @param int $id	新闻ID
-	 */
-	public function getDetail($id){
-		$where['id'] = array('eq', $id);
-		$newsinfo = $this->where($where)->find();
-		return $newsinfo;
-	}
+    /**
+     * 获得最新新闻
+     * 
+     * @param int $id	新闻ID
+     */
+    public function getDetail($id) {
+        $where['id'] = array('eq', $id);
+        $newsinfo = $this->where($where)->find();
+        return $newsinfo;
+    }
 
 }
 
