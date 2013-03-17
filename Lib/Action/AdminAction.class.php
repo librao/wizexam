495c1bd527477f845dd54ff23227c58db6ed2838
Lucky <libtowork@gmail.com>
2013-03-18 01:07:17 +0800

    none

--- Lib/Action/AdminAction.class.php
+++ Lib/Action/AdminAction.class.php
@@ -3,15 +3,28 @@
 class AdminAction extends Action {
 
     public function index() {
-		$news_model = M('News');
+        $news_model = M('News');
         $where['status'] = array('eq', NewsModel::STATUS_ACTIVE);
-        $list = $news_model->where($where)->order('created_at desc')->select();
+        $list = $news_model->where($where)->order('id desc')->select();
         $this->assign('list', $list);
         $this->display();
     }
 
-	public function edit() {
-        $this->display();
+    public function add() {
+        $this->assign('action', U('Admin/insert'));
+        $this->display('form');
+    }
+
+    public function edit() {
+        $model = D('News');
+        $news = $model->getDetail(intval($_REQUEST['id']));
+        if ($news) {
+            $this->assign('news', $news);
+            $this->assign('action', U('Admin/update'));
+            $this->display('form');
+        } else {
+            $this->error('Could not found news!');
+        }
     }
 
     public function insert() {
@@ -26,6 +39,14 @@ class AdminAction extends Action {
         } else {
             $this->error('请填写带*内容');
         }
+    }
+
+    public function update() {
+        
+    }
+
+    public function delete() {
+        $id = intval($_REQUEST['id']);
     }
 
 }
