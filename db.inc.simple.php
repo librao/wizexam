190a24f0fc88294751bc424c121897235f1771db
Lucky <libtowork@gmail.com>
2013-03-17 12:30:23 +0800

    remove some test file and create db config simple

--- db.inc.simple.php
+++ db.inc.simple.php
@@ -4,9 +4,9 @@
 return array(
     'DB_TYPE' => 'mysql', // 数据库类型
     'DB_HOST' => 'localhost', // 服务器地址
-    'DB_NAME' => 'news_system', // 数据库名
-    'DB_USER' => 'root', // 用户名
-    'DB_PWD' => 'librao123', // 密码
+    'DB_NAME' => '', // 数据库名
+    'DB_USER' => '', // 用户名
+    'DB_PWD' => '', // 密码
     'DB_PREFIX' => 'lib_', // 数据库表前缀
 );
 ?>
